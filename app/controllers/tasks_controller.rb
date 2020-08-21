84b65bab315b3500c2d0598ca2ef7b9569c427d4
naru.tech <naru.tech3333@gmail.com>
2020-08-21 09:16:56 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(message_params)
+      @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def update
        @task = Task.find(params[:id])
 
-    if @message.update(message_params)
+    if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
@@ -52,6 +52,6 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
-  
+  end
   
 end
